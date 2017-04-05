03efe9621491c4f13d43902d32b78445f26b80e8
Andrian Putina <anr.putina@gmail.com>
2017-04-05 18:20:08 +0200

    Added class GroupAdapter

--- app/src/main/java/group3/myapplicationlab2/MainActivity.java
+++ app/src/main/java/group3/myapplicationlab2/MainActivity.java
@@ -33,35 +33,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        class GroupAdapter extends ArrayAdapter<Group> {
-
-            public GroupAdapter(Context context, ArrayList<Group> users) {
-                super(context, 0, users);
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                // Get the data item for this position
-                Group group = getItem(position);
-                // Check if an existing view is being reused, otherwise inflate the view
-                if (convertView == null) {
-                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.group_item, parent, false);
-                }
-                // Lookup view for data population
-                TextView groupName = (TextView) convertView.findViewById(R.id.group_item_name);
-                TextView groupDescription = (TextView) convertView.findViewById(R.id.group_item_component);
-                //TextView tvHome = (TextView) convertView.findViewById(R.id.tvHome);
-                // Populate the data into the template view using the data object
-                groupName.setText(group.getName());
-                groupDescription.setText(group.getDescription());
-                //tvHome.setText(user.hometown);
-
-                // Return the completed view to render on screen
-                return convertView;
-            }
-        }
-
         ArrayList<Group> arrayOfGroups = new ArrayList<Group>();
         GroupAdapter adapter = new GroupAdapter(this, arrayOfGroups);
 
