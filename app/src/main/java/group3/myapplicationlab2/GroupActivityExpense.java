7ac48579f1d47993d6106104112602ca5c06fe10
Andrian Putina <anr.putina@gmail.com>
2017-04-05 23:15:22 +0200

    Added possibility to add expense

--- app/src/main/java/group3/myapplicationlab2/GroupActivityExpense.java
+++ app/src/main/java/group3/myapplicationlab2/GroupActivityExpense.java
@@ -1,18 +1,27 @@
 package group3.myapplicationlab2;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GroupActivityExpense extends AppCompatActivity {
+
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +31,45 @@ public class GroupActivityExpense extends AppCompatActivity {
         final int passedVar = getIntent().getIntExtra("groupNumber", 0);
         Log.d("Debug", String.valueOf(passedVar));
 
-        ListView list;
+        String[] spese = {"Expense 1", "Expense 2", "Expense 3"};
+
+        arrayList = new ArrayList<>(Arrays.asList(spese));
+        adapter = new ArrayAdapter<String>(this, R.layout.expense_item, arrayList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                // There is nothing to convert --> I need to create extra view
+                if (convertView==null) {
+                    // Returns a view: inflate is used to populate a view
+                    convertView = getLayoutInflater().inflate(R.layout.expense_item, parent, false);
+                }
+                TextView tv = (TextView)convertView.findViewById(R.id.expense_id);
+                tv.setText(getItem(position).toString());
+
+                TextView author = (TextView) convertView.findViewById(R.id.expense_author);
+                author.setText("Author " + (position+1) + " of group " + (passedVar + 1));
+
+                TextView amount = (TextView) convertView.findViewById(R.id.expense_amount);
+                amount.setText("Amount random");
+
+                TextView date = (TextView) convertView.findViewById(R.id.expense_date);
+                date.setText("Date random");
+
+                return convertView;
+            }
+        };
+
+        final ListView list;
+        list = (ListView)findViewById(R.id.expense_list);
+        list.setAdapter(adapter);
+
+        /*
+        final ListView list;
         list = (ListView)findViewById(R.id.expense_list);
 
         list.setAdapter(new BaseAdapter() {
+
             String[] spese = {"Expense 1", "Expense 2", "Expense 3"};
+
             @Override
             public int getCount() {return spese.length;}
 
@@ -57,7 +100,8 @@ public class GroupActivityExpense extends AppCompatActivity {
 
                 return convertView;
             }
-        });
+
+        });*/
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -66,8 +110,13 @@ public class GroupActivityExpense extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                String newItem = "New Expense";
+                arrayList.add(newItem);
+                adapter.notifyDataSetChanged();
+
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
     }
