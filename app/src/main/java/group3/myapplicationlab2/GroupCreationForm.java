540587555908d3407fb54ace18bf893bf03519c9
Gaetano Moceri <gaetanomoceri@gmail.com>
2017-04-26 20:00:49 +0200

    New group function added

--- app/src/main/java/group3/myapplicationlab2/GroupCreationForm.java
+++ app/src/main/java/group3/myapplicationlab2/GroupCreationForm.java
@@ -3,19 +3,28 @@ package group3.myapplicationlab2;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GroupCreationForm extends AppCompatActivity {
 
+    private ArrayList<String> membersList;
+    private ArrayAdapter<String> membersAdapter;
     private EditText groupName, groupDescription, newParticipant;
+    private List<String> members;
     private FirebaseDatabase mDatabase;
+    private Button btnNewPart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +36,32 @@ public class GroupCreationForm extends AppCompatActivity {
         groupName = (EditText)findViewById(R.id.new_group);
         groupDescription = (EditText)findViewById(R.id.group_description);
         newParticipant = (EditText)findViewById(R.id.new_participant);
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.new_group_btn);
+        btnNewPart = (Button)findViewById(R.id.new_part_btn);
+        final List<String> members = new ArrayList<String>();
 
+        // Insert new participants
+        ListView lw = (ListView)findViewById(R.id.list_part);
+        String[] participants = {};
+        membersList = new ArrayList<>(Arrays.asList(participants));
+        membersAdapter = new ArrayAdapter<String>(this, R.layout.new_member_item, R.id.new_member, participants);
+        lw.setAdapter(membersAdapter);
+        btnNewPart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newP = newParticipant.getText().toString();
+                members.add(newP);
+                /*membersList.add(newP);
+                membersAdapter.notifyDataSetChanged();*/
+                Log.d("Debug", newP);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.new_group_btn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                List<String> members = new ArrayList<>();
-                members.add(newParticipant.getText().toString());
+                //List<String> members = new ArrayList<>();
+                //members.add(newParticipant.getText().toString());
 
                 final Group newGroup = new Group();
                 newGroup.setDescription(groupDescription.getText().toString());
