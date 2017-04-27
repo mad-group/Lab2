3338f88b54034b5657dab4ab63ed6815b118711f
mc <michelecocca.mc@gmail.com>
2017-04-27 09:58:17 +0200

    User class

---
+++ app/src/main/java/group3/myapplicationlab2/User.java
@@ -0,0 +1,36 @@
+package group3.myapplicationlab2;
+
+/**
+ * Created by mc on 26/04/17.
+ */
+
+public class User {
+    private String ID;
+    private String email;
+    private String password;
+
+    public User (String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setPassword(String password){
+        /*check the case of internal spaces/blank/invalid chars */
+        if (password.trim().length() >= 6)
+            this.password = password.trim();
+        return;
+    }
+    public String getPassword(){return password;}
+
+    public void setEmail(String email){
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() ) {
+            this.email = email;
+            return;
+        }
+    }
+    public String getEmail(){return email;}
+
+    public void setID(String ID){this.ID = ID;}
+    public String getId(){return this.ID;}
+
+}
