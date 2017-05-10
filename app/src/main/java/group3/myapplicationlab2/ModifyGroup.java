6326131ce77bbccdd427af3192bfd906ed800ae0
Gaetano Moceri <gaetanomoceri@gmail.com>
2017-05-10 19:56:28 +0200

    Modification group form

---
+++ app/src/main/java/group3/myapplicationlab2/ModifyGroup.java
@@ -0,0 +1,13 @@
+package group3.myapplicationlab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_group);
+    }
+}
